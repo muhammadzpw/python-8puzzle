b5aae6703093b168b2acf3f3cbf677669d2c0dd5
Muhammad Z P W <zhafarudiner@yahoo.com>
2019-11-08 07:29:20 +0700

    Init and add helper functions

---
+++ src/helpers.py
@@ -0,0 +1,95 @@
+def calc_distance(stateA: str, stateB: str): 
+  if len(stateA) != len(stateB):
+    raise Exception("The size of input a and b should be equal.")
+  if len(stateA) != 9:
+    raise Exception("The size of input must be exactly 9")
+  distance = 0
+  for i in range(len(stateA)):
+    if stateA[i] != stateB[i]:
+      distance += 1
+  return distance
+
+def find_space_position(input: str):
+  return input.index('#')
+
+def is_possible_up(input: str):
+  space_position = find_space_position(input)
+  if space_position in [0, 1, 2]:
+    return False
+  return True
+
+def is_possible_left(input: str):
+  space_position = find_space_position(input)
+  if space_position in [0, 3, 6]:
+    return False
+  return True
+
+def is_possible_right(input: str):
+  space_position = find_space_position(input)
+  if space_position in [2, 5, 8]:
+    return False
+  return True
+
+def is_possible_down(input: str):
+  space_position = find_space_position(input)
+  if space_position in [6, 7, 8]:
+    return False
+  return True
+
+def is_move_possible(move, input):
+  if move == "up":
+    return is_possible_up(input)
+  elif move == "left":
+    return is_possible_left(input)
+  elif move == "right":
+    return is_possible_right(input)
+  elif move == "down":
+    return is_possible_down(input)
+  else:
+    raise Exception("Unknown move")
+
+def move_up(input: str):
+  if not is_possible_up(input):
+    return None
+  space_position = find_space_position(input)
+  switch_space_position = space_position - 3
+  return switch_position(input, space_position, switch_space_position)
+
+def move_down(input:str):
+  if not is_possible_down(input):
+    return None
+  space_position = find_space_position(input)
+  switch_space_position = space_position + 3
+  return switch_position(input, space_position, switch_space_position)
+
+def move_left(input: str):
+  if not is_possible_left(input):
+    return None
+  space_position = find_space_position(input)
+  switch_space_position = space_position - 1
+  return switch_position(input, space_position, switch_space_position)
+
+def move_right(input: str):
+  if not is_possible_right(input):
+    return None
+  space_position = find_space_position(input)
+  switch_space_position = space_position + 1
+  return switch_position(input, space_position, switch_space_position)
+
+def switch_position(input: str, idx: int, switch_idx: int):
+  output = list(input)
+  tmp = output[switch_idx]
+  output[switch_idx] = output[idx]
+  output[idx] = tmp
+  return list_to_str(output)
+
+def list_to_str(lst: list):
+  out = ""
+  for x in lst:
+    out += x
+  return out
+
+# print(move_up("123#45678"))
+# print(move_right("123#45678"))
+# print(move_down("123#45678"))
+# print(move_left("123#45678"))
